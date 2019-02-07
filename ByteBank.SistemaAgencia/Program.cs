6701e273119cb3f51c0393b0dc98737dfb333726
Ricardo Martines <rictines@hotmail.com>
2019-02-07 20:59:17 -0200

    07/02/2019

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ByteBank.Modelos;
+
 
 namespace ByteBank.SistemaAgencia
 {
@@ -10,7 +12,11 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            
+            ContaCorrente conta = new ContaCorrente(123, 332211);
+            {
+                Console.WriteLine("O número da agencia é: " + conta.Agencia);
+                Console.WriteLine("O número da conta é: " + conta.Numero);
+            }
         }
     }
 }
