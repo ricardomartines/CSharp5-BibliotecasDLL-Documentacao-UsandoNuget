26ad0e0267c4f6316b5b1dde1d42aab05cc55c1e
Ricardo Martines <rictines@hotmail.com>
2019-02-18 20:59:40 -0300

    18/02/2019

--- ByteBank.Modelos/Funcionarios/FuncionarioAutenticavel.cs
+++ ByteBank.Modelos/Funcionarios/FuncionarioAutenticavel.cs
@@ -9,10 +9,11 @@ namespace ByteBank.Modelos.Funcionarios
 {
     public abstract class FuncionarioAutenticavel : Funcionario, IAutenticavel
     {
+        private AutenticacaoHelper _autenticacaoHelper = new AutenticacaoHelper();
+
         public string Senha { get; set; }
 
-        public FuncionarioAutenticavel(double salario, string cpf)
-            : base(salario, cpf)
+        public FuncionarioAutenticavel(double salario, string cpf) : base(salario, cpf)
         {
         }
 
