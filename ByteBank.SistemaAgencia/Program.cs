f96cd590f420ee4cd0101a31f8c397c4a2b020dc
Ricardo Martines <rictines@hotmail.com>
2019-02-25 21:42:46 -0300

    20/02/2019

--- ByteBank.SistemaAgencia/Program.cs
+++ ByteBank.SistemaAgencia/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ByteBank.Modelos;
+using Humanizer;
 
 
 namespace ByteBank.SistemaAgencia
@@ -12,11 +13,26 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            ContaCorrente conta = new ContaCorrente(123, 332211);
-            {
-                Console.WriteLine("O número da agencia é: " + conta.Agencia);
-                Console.WriteLine("O número da conta é: " + conta.Numero);
-            }
+            /*  ContaCorrente conta = new ContaCorrente(123, 332211);
+              {
+                  Console.WriteLine("O número da agencia é: " + conta.Agencia);
+                  Console.WriteLine("O número da conta é: " + conta.Numero);
+              }
+              */
+
+            DateTime dataFimPagamento = new DateTime(2019, 10, 15);
+
+            DateTime dataCorrente = DateTime.Now;
+
+            Console.WriteLine("Data corrente: " + dataCorrente);
+
+            TimeSpan diferenca = dataFimPagamento - dataCorrente;
+
+            string mensagem = "Vencimento em: " + TimeSpanHumanizeExtensions.Humanize(diferenca);
+
+            Console.WriteLine(mensagem);
+
+            Console.WriteLine("Vencimento em: " + diferenca);
             
         }
     }
